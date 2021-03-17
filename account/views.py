b7d0e46dbe69330a0f726e643536ec1309538eea
biroghlan <biroghlan95@gmail.com>
2021-03-17 14:20:28 +0330

    Finish the sing up with email and reset password

--- account/views.py
+++ account/views.py
@@ -60,3 +60,51 @@ class Login(LoginView):
         else:
             return reverse_lazy('account:profile')
 
+
+
+
+from django.http import HttpResponse
+from .forms import SignupForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+
+class Register(CreateView):
+    form_class=SignupForm
+    template_name="registration/register.html"
+    
+    def form_valid(self,form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        current_site = get_current_site(self.request)
+        mail_subject = 'فعال سازی اکانت'
+        message = render_to_string('registration/activate_account.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+            'token':account_activation_token.make_token(user),
+        })
+        to_email = form.cleaned_data.get('email')
+        email = EmailMessage(
+                    mail_subject, message, to=[to_email]
+        )
+        email.send()
+        return HttpResponse('لینک فعال سازی به ایمیل شما ارسال شد.')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponse('ثبت نام شما با موفقت انجام شد برای <a href="/login/">کلیک</a> ورود کنید.')
+    else:
+        return HttpResponse('این لینک فعال سازی منقضی شده است .لطفا <a href="/register/">دوباره امتحان کنید.</a>')
--- config/urls.py
+++ config/urls.py
@@ -15,14 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from account.views import Login
+from account.views import Login,Register,activate
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls') ),
     path('', include('django.contrib.auth.urls') ),
     path('login/', Login.as_view(), name='login'),
     path('account/', include('account.urls') ),
+    path('register/', Register.as_view(), name='register'),
+    # re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',activate, name='activate'),
+    path('activate/<uidb64>/<token>/', activate,name='activate'),
 ]
+
 
 from django.conf import settings
 from django.conf.urls.static import static
